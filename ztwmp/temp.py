5bceeffd97a6220c3cc93dbce9b5d19289ad09d7
Ojas Patil <128805590+Patil-Ojas@users.noreply.github.com>
2023-10-26 01:09:39 +0530

    Add files via upload

---
+++ ztwmp/temp.py
@@ -0,0 +1,16 @@
+a = int(input("enter no 1"))
+b = int(input("enter no 2"))
+
+avg = (a+b)/2
+print("avg is ", avg)
+
+if a>b:
+    print("a is greater")
+else:
+    print("b is greater")
+
+print("square of a is ", a*a)
+print("square of b is ", b*b)
+
+
+
