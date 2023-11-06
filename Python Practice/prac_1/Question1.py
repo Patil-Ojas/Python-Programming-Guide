0c882683d0cf6b945a86f7b4af1f58bf50b9b9b2
Ojas Patil <128805590+Patil-Ojas@users.noreply.github.com>
2023-11-06 15:15:21 +0530

    Add files via upload

---
+++ Python Practice/prac_1/Question1.py
@@ -0,0 +1,5 @@
+# Addition of two nos
+a = float(input('The first no'))
+b = float(input('The second no'))
+sum = a+b
+print("the addition of the given numbers is ", sum)
---
+++ Python Practice/prac_1/question2.py
@@ -0,0 +1,7 @@
+# find simple interest
+
+p = float(input('Enter the principal amount: '))
+r = float(input('Enter the rate: '))
+t = float(input('Enter the time: '))
+si = (p*r*t)/100
+print("Simple Interest is:", si)
